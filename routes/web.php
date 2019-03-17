c2e0942bd26f329ede2e01ea41a56f1e8c8f11db
lijian <li2515104337@gmail.com>
2019-03-17 13:22:22 +0000

    Add account activations

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::resource('users', 'UsersController');
 Route::get('login', 'SessionsController@create')->name('login');//显示登录页面
 Route::post('login', 'SessionsController@store')->name('store');//创建新会话
 Route::delete('logout', 'SessionsController@destory')->name('logout');//销毁回话
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
